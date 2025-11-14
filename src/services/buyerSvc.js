4886d50d82e50d330366c1446c7bd30dc1b20cc7
Aufa <aufa_romeo@live.com>
2025-11-14 21:49:08 +0800

    FEAT: trycatch and log for buyerSvc

--- src/services/buyerSvc.js
+++ src/services/buyerSvc.js
@@ -2,23 +2,35 @@ import BuyerModel from "../models/BuyerModel.js";
 
 import getTimestamp from "../utils/timestamp.js";
 
-export async function getBuyerByNameAndPlatform(buyerName, platform) {
-  const buyer = await BuyerModel.findOne({
-    buyerName,
-    platform,
-  });
-  if (buyer) console.log(`[${getTimestamp()}] Buyer exists: ${buyer._id}`);
-  return buyer;
+export async function getBuyerByNameAndPlatformSvc({ buyerName, platform, caller = null }) {
+  try {
+    const buyer = await BuyerModel.findOne({
+      buyerName,
+      platform,
+    });
+    if (buyer) console.log(`[${getTimestamp()}] [${caller}] [getBuyerByNameAndPlatform] Buyer found: ${buyer._id}`);
+    return buyer;
+    
+  } catch (e) {
+    console.error(`[${getTimestamp()}] [${caller}] [getBuyerByNameAndPlatform] getBuyerByNameAndPlatform FAILED:`, e); 
+    throw e;     
+  }
 }
 
-export async function createNewBuyer({buyerName, platformId = null, platform, nickname = null, createdAt = Date.now()}) {
-  const newBuyer = await BuyerModel.create({
-    buyerName,
-    platformId,
-    platform,
-    nickname,
-    createdAt,
-  });
-  if (newBuyer) console.log(`[${getTimestamp()}] New buyer created: ${newBuyer._id}`);
-  return newBuyer;
+export async function createNewBuyerSvc({ buyerName, platformId = null, platform, nickname = null, createdAt = Date.now(), caller = null }) {
+  try {
+    const newBuyer = await BuyerModel.create({
+      buyerName,
+      platformId,
+      platform,
+      nickname,
+      createdAt,
+    });
+    if (newBuyer) console.log(`[${getTimestamp()}] [${caller}] [createNewBuyer] New buyer created: ${newBuyer._id}`);
+    return newBuyer;
+    
+  } catch (e) {
+    console.error(`[${getTimestamp()}] [${caller}] [createNewBuyer] createNewBuyer FAILED:`, e); 
+    throw e;     
+  }
 }
