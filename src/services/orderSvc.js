90c0689d981e518b9004a61296804b483a0d6b41
Aufa <aufa_romeo@live.com>
2025-11-14 21:51:04 +0800

    FEAT: trycatch and log for orderSvc

--- src/services/orderSvc.js
+++ src/services/orderSvc.js
@@ -1,39 +1,48 @@
-
-import BuyerModel from "../models/BuyerModel.js";
 import ProductModel from "../models/ProductModel.js";
 import OrderModel from "../models/OrderModel.js";
 
 import getTimestamp from "../utils/timestamp.js";
 
-export async function createOrderSvc({buyerId, productCode, quantity, orderDate = Date.now()}) {
-  
-  const product = await ProductModel.findOne({
-    productCode,
-    status: "active",
-  });
-  
-  if (!product) {
-    console.log(`Product ${productCode} not found or inactive`);
-    return {isFailedOrder: true};
-  }
-  
-  if (product.stockType === "SOH" && product.stock < quantity) {
-    return {isFailedOrder: true};
-  }
+export async function createOrderSvc({ buyerId, productCode, quantity, orderDate = Date.now(), caller = null}) {
 
-  const order = await OrderModel.create({
-    buyer: buyerId,
-    product: product._id,
-    quantity,
-    orderDate: Date.now(),
-  });
+  try {
+    const product = await ProductModel.findOne({ // DB ACCESS PRODUCTS
+      productCode,
+      status: "active",
+    }); 
+    
+    if (!product) {
+      console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Product ${productCode} not found or inactive`);
+      return {isFailedOrder: true};
+    } else {
+      console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Product ${productCode} found`);
+    }
+    
+    if (product.stockType === "SOH" && product.stock < quantity) {
+      console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Product ${productCode} insufficient stock quantity`);
+      return {isFailedOrder: true};
+    }
 
-  if (product.stockType === "SOH") {
-    await ProductModel.updateOne(
-      { _id: product._id, stock: { $gte: quantity } },
-      { $inc: { stock: -quantity } }
-    );
+    const order = await OrderModel.create({ // DB ACCESS ORDERS
+      buyer: buyerId,
+      product: product._id,
+      quantity,
+      orderDate: Date.now(),
+    });
+    console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Order created: ${order._id}`);
+
+    if (product.stockType === "SOH") { // DB ACCESS PRODUCTS
+      await ProductModel.updateOne(
+        { _id: product._id, stock: { $gte: quantity } },
+        { $inc: { stock: -quantity } }
+      );
+      console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Product stock quantity reduced by: ${quantity}`);
+    }
+
+    return {_id: order._id, isFailedOrder: false};
+    
+  } catch (e) {
+    console.error(`[${getTimestamp()}] [${caller}] [createOrderSvc] createOrderSvc FAILED:`, e); 
+    throw e;   
   }
-
-  return {_id: order._id, isFailedOrder: false};
 }
