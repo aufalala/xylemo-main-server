e60c89b05375eaeeb4f157a7740d33ebffb76243
Aufa <aufa_romeo@live.com>
2025-11-14 20:48:23 +0800

    FEAT: added timestamp to log

--- src/workers/_workerEventListener.js
+++ src/workers/_workerEventListener.js
@@ -1,28 +1,30 @@
+import getTimestamp from "../utils/timestamp.js";
+
 export function workerEventListeners(workerName, workerInstance) {
   const handleWorkerEvent = (event, ...args) => {
     switch (event) {
       case "completed":
         const [job, result] = args;
-        console.log(`[${workerName}] Job ${job.id} completed. Result:`, result);
+        console.log(`[${getTimestamp()}] [${workerName}] Job [${job.id}] completed. Result:`, result);
         break;
       case "failed":
         const [jobFailed, err] = args;
-        console.error(`[${workerName}] Job ${jobFailed.id} failed:`, err);
+        console.error(`[${getTimestamp()}] [${workerName}] Job [${jobFailed.id}] failed:`, err);
         break;
       case "stalled":
         const [jobStalled] = args;
-        console.warn(`[${workerName}] Job ${jobStalled.id} stalled!`);
+        console.warn(`[${getTimestamp()}] [${workerName}] Job [${jobStalled.id}] stalled!`);
         break;
       case "active":
         const [jobActive] = args;
-        console.log(`[${workerName}] Job ${jobActive.id} started.`);
+        console.log(`[${getTimestamp()}] [${workerName}] Job [${jobActive.id}] started.`);
         break;
       case "waiting":
         const [jobId] = args;
-        console.log(`[${workerName}] Job ${jobId} is waiting.`);
+        console.log(`[${getTimestamp()}] [${workerName}] Job [${jobId}] is waiting.`);
         break;
       default:
-        console.warn(`[${workerName}] Unhandled event: ${event}`);
+        console.warn(`[${getTimestamp()}] [${workerName}] Unhandled event: ${event}`);
     }
   };
 
