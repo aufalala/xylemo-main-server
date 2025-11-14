e927958504aaf4a7cb522f6b4c089056d0268e59
Aufa <aufa_romeo@live.com>
2025-11-14 21:49:36 +0800

    FEAT: trycatch and log for chatrSvc

--- src/services/chatSvc.js
+++ src/services/chatSvc.js
@@ -3,23 +3,29 @@ import ChatModel from "../models/ChatModel.js";
 
 import getTimestamp from "../utils/timestamp.js";
 
-export async function checkChatOrderSvc(text) {
-  if (!text || typeof text !== "string") {
-    return { isOrder: false, productCode: null, quantity: null };
+export async function checkChatOrderSvc({ text, caller }) {
+  try {
+    if (!text || typeof text !== "string") {
+      console.log(`[${getTimestamp()}] [${caller}] [checkChatOrderSvc] Invalid Chat Text`);
+      return { isOrder: false, productCode: null, quantity: null };
+    }
+    
+    const trimmed = text.trim();
+    const match = trimmed.match(/^([A-Z]+)\+(\d+)$/);
+    
+    if (!match) {
+      console.log(`[${getTimestamp()}] [${caller}] [checkChatOrderSvc] Chat is not an order`);
+      return { isOrder: false, productCode: null, quantity: null };
+    }
+    
+    const productCode = match[1];
+    const quantity = parseInt(match[2], 10);
+    
+    console.log(`[${getTimestamp()}] [${caller}] [checkChatOrderSvc] Chat is an order`);
+    return { isOrder: true, productCode, quantity };
+    
+  } catch (e) {
+    console.error(`[${getTimestamp()}] [${caller}] [checkChatOrderSvc] checkChatOrderSvc FAILED:`, e);
+    throw e;
   }
-
-  const trimmed = text.trim();
-
-  const match = trimmed.match(/^([A-Z]+)\+(\d+)$/);
-
-  if (!match) {
-    console.log(`[${getTimestamp()}] Chat is not an order`);
-    return { isOrder: false, productCode: null, quantity: null };
-  }
-
-  const productCode = match[1];
-  const quantity = parseInt(match[2], 10);
-
-  console.log(`[${getTimestamp()}] Chat is an order`);
-  return { isOrder: true, productCode, quantity };
 }
