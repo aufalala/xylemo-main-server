e6433e2beb4c25f2e9e12849c47df05b18f19b1b
Aufa <aufa_romeo@live.com>
2025-11-15 22:14:55 +0800

    FEAT: env config, timezone for dayjs

--- src/config/_env.js
+++ src/config/_env.js
@@ -4,3 +4,4 @@ dotenv.config();
 export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const TIMEZONE = process.env.TIMEZONE;
