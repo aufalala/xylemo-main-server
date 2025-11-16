3a7f7ef8b53cb92bd3c0eaa4ba5fbb49097fd861
Aufa <aufa_romeo@live.com>
2025-11-16 23:54:15 +0800

    FEAT: env config, added access and refresh token secrets/expires

--- src/config/_env.js
+++ src/config/_env.js
@@ -3,5 +3,11 @@ dotenv.config();
 
 export const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 export const MONGODB_URI = process.env.MONGODB_URI;
-export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+
+export const ACCESS_TOKEN_EXPIRES = process.env.ACCESS_TOKEN_EXPIRES;
+export const REFRESH_TOKEN_EXPIRES = process.env.REFRESH_TOKEN_EXPIRES;
+
 export const TIMEZONE = process.env.TIMEZONE;
