c2dc2d120e7ef06062493843f29b7f216706a3c2
Aufa <aufa_romeo@live.com>
2025-11-28 03:32:38 +0800

    FEAT: created new order broadcast. get order svc.

--- src/services/orderSvc.js
+++ src/services/orderSvc.js
@@ -2,17 +2,43 @@ import ProductModel from "../models/ProductModel.js";
 import OrderModel from "../models/OrderModel.js";
 
 import { getTimestamp } from "../utils/timestamp.js";
+import { broadcastEvent } from "../sse/sseManager.js";
+
+
+export async function getOrdersSvc({filter = null}) {
+  try {
+    const orders = await OrderModel.find(filter).populate([
+      { path: "buyer" },
+      { path: "product" },
+    ]);
+    return orders;
+
+  } catch (e) {
+    console.error(`[${getTimestamp()}] getOrdersSvc FAILED:`, e);
+    throw e; 
+  }
+}
 
 export async function createOrderSvc({ buyerId, productId, quantity, orderDate = Date.now(), caller = null}) {
-
   try {
-    const order = await OrderModel.create({ // DB ACCESS ORDERS
+    let order = await OrderModel.create({ // DB ACCESS ORDERS
       buyer: buyerId,
       product: productId,
       quantity,
       orderDate,
     });
+
+    order = await order.populate([
+      { path: "buyer" },
+      { path: "product" }
+    ]);
+
     console.log(`[${getTimestamp()}] [${caller}] [createOrderSvc] Order created: ${order._id}`);
+
+    broadcastEvent({
+      type: "new_order",
+      order: order,
+    }, "createOrderSvc");
 
     return order;
     
